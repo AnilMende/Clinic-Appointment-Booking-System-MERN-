71742041525ca1fc6ed394bd414104213ae194cf
Anil <anilmende2002@gmail.com>
2026-04-06 01:37:19 +0530

    added transporter for send email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,20 +2,23 @@ import nodemailer from 'nodemailer';
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,            // ✅ 587 instead of 465
-    secure: false,        // ✅ false for STARTTLS
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS   // ✅ Must be Gmail App Password
-    },
-    tls: {
-        rejectUnauthorized: false      // ✅ Helps on cloud environments
-    }
-});
 
 export const sendEmail = async ({ to, subject, text }) => {
+    
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,            // ✅ 587 instead of 465
+        secure: false,        // ✅ false for STARTTLS
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS   // ✅ Must be Gmail App Password
+        },
+        tls: {
+            rejectUnauthorized: false      // ✅ Helps on cloud environments
+        }
+    });
+
+
     try {
         // ✅ Verify SMTP connection before sending
         await transporter.verify();
