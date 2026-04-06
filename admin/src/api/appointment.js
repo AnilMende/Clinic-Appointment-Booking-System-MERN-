bcdfc047fda4466c968d2b498818ed817659e087
Anil <anilmende2002@gmail.com>
2026-04-06 11:10:44 +0530

    changed backend url

--- admin/src/api/appointment.js
+++ admin/src/api/appointment.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "https://backend-clinic-appointment.onrender.com/api"
+    baseURL : "http://localhost:5000/api"
 })
 
 
--- admin/src/pages/Auth/Login.jsx
+++ admin/src/pages/Auth/Login.jsx
@@ -7,7 +7,8 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
 
-    const backend_url = "https://backend-clinic-appointment.onrender.com/api"
+    //const backend_url = "https://backend-clinic-appointment.onrender.com/api"
+    const backend_url = "http://localhost:5000/api";
 
     const [form, setForm] = useState({ email: "", passowrd: "" });
 
