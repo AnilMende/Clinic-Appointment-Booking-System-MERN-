6bdf39adaae7120c7a67d7643d7a1b3691c89dc9
Anil <anilmende2002@gmail.com>
2026-04-06 11:11:27 +0530

    fixed logging

--- frontend/src/components/BookingForm/BookingForm.jsx
+++ frontend/src/components/BookingForm/BookingForm.jsx
@@ -6,8 +6,8 @@ import toast from "react-hot-toast";
 
 const BookingForm = () => {
 
-    //const base_url = "http://localhost:5000/api";
-    const base_url = "https://backend-clinic-appointment.onrender.com/api"
+    const base_url = "http://localhost:5000/api";
+    //const base_url = "https://backend-clinic-appointment.onrender.com/api"
 
     //when the reponse is submitting the button will be in loading state i.e.. Booking
     const [loading, setLoading] = useState(false);
